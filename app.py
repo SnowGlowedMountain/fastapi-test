9167eed03a76c1d9aea6d34cc84c00e65d87b394
Serhii <snow55mountain@gmail.com>
2024-08-20 07:50:38 +0300

    init

---
+++ app.py
@@ -0,0 +1,19 @@
+import fastapi, uvicorn
+from starlette.requests import Request
+import os
+
+api = fastapi.FastAPI()
+
+@api.get('/hello')
+def hello(request: Request):
+    return "Hello World!"
+
+if __name__ == "__main__":
+    print("Starting webserver...")
+    uvicorn.run(
+        api,
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 8080)),
+        log_level=os.getenv('LOG_LEVEL', "info"),
+        proxy_headers=True
+    )
