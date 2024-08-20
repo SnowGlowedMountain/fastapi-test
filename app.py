443ad7cc01a74ff2406bb36d6292c3f8c5340f09
Serhii Ofii <132130496+SnowGlowedMountain@users.noreply.github.com>
2024-08-20 22:22:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
 import fastapi, uvicorn
-from starlette.requests import Request
 import os
 
 api = fastapi.FastAPI()
 
-@api.get('/hello')
-def hello(request: Request):
-    return "Hello World!"
+@app.get("/")
+async def root() -> dict[str, str]:
+    return {"message": "Hello, World!"}
 
 if __name__ == "__main__":
     print("Starting webserver...")
-    uvicorn.run(
-        api,
-        host="0.0.0.0",
-        port=int(os.getenv("PORT", 8080)),
-        log_level=os.getenv('LOG_LEVEL', "info"),
-        proxy_headers=True
-    )
+    uvicorn.run(app, port=8080, host='0.0.0.0')
