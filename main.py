8ff9c68b636e4ef1f4e3fb9e44a7867355bda93f
Serhii <snow55mountain@gmail.com>
2024-08-20 06:39:03 +0300

    update

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-if __name__ == "__main__":
-    print("Starting webserver...")
-    uvicorn.run(app, port=8080, host='0.0.0.0')
+    return {"message": "Hello, World!"}
